da0306df086a5b3c561ab524194c23ae368b15d0
edwin <molinaedwin806@gmail.com>
2015-10-22 15:19:31 -0430

    cambio inicio reposit orio

---
+++ polls/migrations/0002_xxxxxxx_zzzzzzzz.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='xxxxxxx',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question_text', models.CharField(max_length=20)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='zzzzzzzz',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('choice_text', models.CharField(max_length=20)),
+                ('votes', models.IntegerField(default=0)),
+                ('question', models.ForeignKey(to='polls.Question')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
